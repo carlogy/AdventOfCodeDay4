70c0ccd295b1f077a9b8e7555048eb1e75480f9c
Carlo Yannuzzi <carlogy@Carlos-MacBook-Pro.local>
2023-06-18 11:18:53 -0400

    final solution

---
+++ Day4AOC/CleanUp.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+
+namespace Day4AOC
+{
+	public class CleanUp
+	{
+		public static int Groups()
+		{
+			Console.Write("Input PuzzleInput path: ");
+			var path = Console.ReadLine();
+
+			var SectionAssignments = File.ReadAllLines(path);
+
+			var OverlappingAssignments = 0;
+			var Total = 0;
+
+
+
+			foreach (var line in SectionAssignments)
+			{
+				var indexComma = line.IndexOf(",");
+
+				var firstSection = line.Substring(0, indexComma);
+				var secondSection = line.Substring(indexComma + 1);
+
+
+				var startOne = Convert.ToInt32(firstSection.Split('-')[0]);
+				var endOne = Convert.ToInt32(firstSection.Split('-')[1]);
+				var startTwo = Convert.ToInt32(secondSection.Split('-')[0]);
+				var endTwo = Convert.ToInt32(secondSection.Split('-')[1]);
+
+
+
+
+
+
+				if (startTwo >= startOne && endTwo <= endOne)
+				{
+					OverlappingAssignments++;
+				}
+				else if (startOne >= startTwo && endOne <= endTwo)
+				{
+					OverlappingAssignments++;
+				}
+
+				if (endOne >= startTwo && endTwo >= startOne)
+				{
+					Total++;
+				}
+
+
+				
+			
+
+				
+				
+            }
+
+
+
+			return Total;
+		}
+	}
+}
+
---
+++ Day4AOC/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Day4AOC;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine(CleanUp.Groups());
+        Console.ReadLine();
+    }
+}
+
